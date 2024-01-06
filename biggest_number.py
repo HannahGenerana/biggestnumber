24a216a39ea7909e6775f036dae58867eb3a8aa2
HannahGenerana <hannahgenerana@gmail.com>
2024-01-06 19:07:11 +0800

    pseudocode

---
+++ biggest_number.py
@@ -0,0 +1,13 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+# pseudocode
+
+print ("Hello! Kindy input some random numbers below to test if the code is correct.")
+
+# ask the user to input 3 numbers
+first_num = int(input("Kindly input your first random number: "))
+second_num = int(input("Kindly input your second random number: "))
+third_num = int(input("Kindly input your third random number: "))
+
+# check if biggest number
+# print the biggest number
