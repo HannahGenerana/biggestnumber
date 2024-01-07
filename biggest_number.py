2fb34ac1efa2280353987e8cec6105eb93ef5511
HannahGenerana <hannahgenerana@gmail.com>
2024-01-07 19:50:43 +0800

    fixing the design

--- biggest_number.py
+++ biggest_number.py
@@ -10,7 +10,7 @@ init(convert=True)
 
 # make a window that ask the user to input random numbers
 customtkinter.set_appearance_mode("Light")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
 root.geometry("350x380")
@@ -37,13 +37,13 @@ def inputs():
   # check and find the biggest number then print the biggest number
     if first_number >= second_number and first_number >= third_number:
         time.sleep(1.30)
-        print (first_number,Fore.MAGENTA + Style.BRIGHT, "is the largest number among all inputs") 
+        print (Fore.MAGENTA + Style.BRIGHT,first_number, "is the largest number among all inputs") 
     elif second_number >= first_number and second_number >= third_number:
         time.sleep(1.30)
-        print (second_number, Fore.MAGENTA + Style.BRIGHT, "is the largest number among all inputs")
+        print (Fore.MAGENTA + Style.BRIGHT,second_number,  "is the largest number among all inputs")
     else:
         time.sleep(1.30)
-        print (third_number, Fore.MAGENTA + Style.BRIGHT, "is the largest number among all inputs") 
+        print (Fore.MAGENTA + Style.BRIGHT,third_number,  "is the largest number among all inputs") 
 
 
   # close the window after showing the biggest number
@@ -54,10 +54,10 @@ def inputs():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20,padx=20,fill="both", expand=True)
 
-label_one= customtkinter.CTkLabel(master=frame, text="Welcome!")
+label_one= customtkinter.CTkLabel(master=frame, text="Welcome!", font=("Helvetica", 25, "bold"))
 label_one.pack(pady=5,padx=5)
 
-label_two= customtkinter.CTkLabel(master=frame, text="Kindly input 3 random numbers")
+label_two= customtkinter.CTkLabel(master=frame, text="Kindly input 3 random numbers", font=("Helvetica",20, "italic"))
 label_two.pack(pady=5,padx=5)
 
 entry_first = customtkinter.CTkEntry(master=frame, placeholder_text="Enter any random number")
