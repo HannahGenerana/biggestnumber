2ecb3884412e733eb9d7c33c2c2b65718d0b92a4
HannahGenerana <hannahgenerana@gmail.com>
2024-01-07 17:33:22 +0800

    time sleep per print

--- biggest_number.py
+++ biggest_number.py
@@ -13,7 +13,7 @@ customtkinter.set_appearance_mode("Light")
 customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
-root.geometry("300x300")
+root.geometry("400x400")
 
 
   # ask the user to input 3 numbers
@@ -21,19 +21,24 @@ def inputs():
     first_number = int(entry_first.get())
     second_number = int(entry_second.get())
     third_number = int(entry_third.get())
+    time.sleep(1.30)
     print (Fore.LIGHTYELLOW_EX + Style.NORMAL,"First number: ",first_number)
+    time.sleep(1.40)
     print (Fore.LIGHTBLUE_EX + Style.NORMAL,"Second number: ",second_number)
+    time.sleep(1.50)
     print (Fore.LIGHTGREEN_EX + Style.NORMAL,"Third number: ",third_number)
     
 
-
   # check and find the biggest number then print the biggest number
     if first_number >= second_number and first_number >= third_number:
-         print (first_number,Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs") 
+        time.sleep(1.30)
+        print (first_number,Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs") 
     elif second_number >= first_number and second_number >= third_number:
-         print (second_number, Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs")
+        time.sleep(1.30)
+        print (second_number, Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs")
     else:
-         print (third_number, Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs") 
+        time.sleep(1.30)
+        print (third_number, Fore.MAGENTA + Style.BRIGHT, "is the largest among all inputs") 
 
 
   # close the window after showing the biggest number
@@ -42,20 +47,22 @@ def inputs():
 
 # custom the window
 frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=15,padx=25,fill="both", expand=True)
+frame.pack(pady=30,padx=30,fill="both", expand=True)
 
+label_one= customtkinter.CTkLabel(master=frame, text="Welcome!")
+label_one.pack(pady=5,padx=5)
 
-label = customtkinter.CTkLabel(master=frame, text="Input 3 random numbers")
-label.pack(pady=10,padx=10)
+label_two= customtkinter.CTkLabel(master=frame, text="Input 3 random numbers")
+label_two.pack(pady=15,padx=15)
 
-entry_first = customtkinter.CTkEntry(master=frame, placeholder_text="First Number")
-entry_first.pack(pady=10,padx=10)
+entry_first = customtkinter.CTkEntry(master=frame, placeholder_text="Enter a random number")
+entry_first.pack(pady=15,padx=20)
 
-entry_second = customtkinter.CTkEntry(master=frame, placeholder_text="Second Number")
-entry_second.pack(pady=10,padx=10)
+entry_second = customtkinter.CTkEntry(master=frame, placeholder_text="Enter a random number")
+entry_second.pack(pady=15,padx=20)
 
-entry_third = customtkinter.CTkEntry(master=frame, placeholder_text="Third Number")
-entry_third.pack(pady=10,padx=10)
+entry_third = customtkinter.CTkEntry(master=frame, placeholder_text="Enter a random number")
+entry_third.pack(pady=15,padx=15)
 
 button = customtkinter.CTkButton(master=frame, text="Find the largest input number", command=inputs,)
 button.pack(pady=25,padx=25)
