906bb8b7191c33e2931d7061ad231e8c55d348b1
HannahGenerana <hannahgenerana@gmail.com>
2024-01-07 11:42:18 +0800

    showing the largest number

--- biggest_number.py
+++ biggest_number.py
@@ -5,31 +5,7 @@ from tkinter import messagebox
 from tkinter import*
 
 # ask the user to input 3 numbers
-window=Tk()
-window.title("Random Number")
-window.geometry("600x200")
-
-l1=Label(window, text="Kindly input some random numbers below.", font=(14))
-l2=Label(window,text="First Number:", font=(14))
-l3=Label(window, text="Second Number:", font=(14))
-l4=Label(window,text="Third Number:", font=(14))
-l1.grid(row=0,column=0,padx=5,pady=5)
-l2.grid(row=1,column=0,padx=5,pady=5)
-l3.grid(row=2,column=0,padx=5,pady=5)
-l4.grid(row=3,column=0,padx=5,pady=5)
-
-first_num=StringVar()
-second_num=StringVar()
-third_num=StringVar()
-t1=Entry(window,textvariable=first_num,font=(14))
-t2=Entry(window,textvariable=second_num,font=(14))
-t3=Entry(window,textvariable=third_num,font=(14))
-t1.grid(row=1,column=1)
-t2.grid(row=2,column=1)
-t3.grid(row=3,column=1)
-
 messagebox.showinfo(title="Random Number", message="Welcome! Kindly click 'OK' to proceed.")
-
 
 first_num = int(input("Kindly input your first random number: "))
 second_num = int(input("Kindly input your second random number: "))
